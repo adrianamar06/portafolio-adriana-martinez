2d4e1afa3e3f4388ee67a98263f42e1389bfc642
Adriana Martinez <adrianamelissa06@gmail.com>
2026-05-28 12:44:58 -0400

    fix: match model import casing for Linux compatibility

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Import Database Models
-const Booking = require('./models/Booking');
-const User = require('./models/User');
+const Booking = require('./models/booking');
+const User = require('./models/user');
 
 // Middleware Configs
 app.use(express.urlencoded({ extended: true }));
